26cee6db87543ec78f0f737f104fd9ba51600b7a
UlianaDi <99318121+UlianaDi@users.noreply.github.com>
2022-04-20 23:17:50 +0300

    Add files via upload

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Батько: Дячок Іван Михайлович, 1983 р.н.,безобітний;" );
+    System.out.println("Мати: Дячок Любов Степанівна, 1984 р.н., продавець-консультант АТБ;");
+    System.out.println("Сестра: Дячок Андріана Іванівна, 2006 р.н., школярка.");
+    }
+}
